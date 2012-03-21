7483ac4a83cab722c3f7f855346d3858c237fdee
Olaf Alders <olaf@wundersolutions.com>
2012-03-21 00:03:51 -0400

    adds ability to exclude paths

--- lib/Plack/Middleware/CSRFBlock.pm
+++ lib/Plack/Middleware/CSRFBlock.pm
@@ -9,13 +9,14 @@ use Plack::TempBuffer;
 use Plack::Util;
 use Digest::SHA1;
 use Plack::Util::Accessor qw(
-    parameter_name token_length session_key blocked onetime
+    parameter_name token_length session_key blocked onetime exclude_paths
     _param_re _token_generator
 );
 
 sub prepare_app {
     my $self = shift;
 
+    $self->exclude_paths([]) unless defined $self->exclude_paths;
     $self->parameter_name('SEC') unless defined $self->parameter_name;
     $self->token_length(16) unless defined $self->token_length;
     $self->session_key('csrfblock.token') unless defined $self->session_key;
@@ -52,11 +53,12 @@ sub call {
     }
 
     # input filter
-    if(
-        $env->{REQUEST_METHOD} =~ m{^post$}i and
-        ($env->{CONTENT_TYPE} =~ m{^(application/x-www-form-urlencoded)}i or
-         $env->{CONTENT_TYPE} =~ m{^(multipart/form-data)}i)
-    ) {
+    if (   !$self->exclude( $env )
+        and $env->{REQUEST_METHOD} =~ m{^post$}i
+        and ( $env->{CONTENT_TYPE} =~ m{^(application/x-www-form-urlencoded)}i
+            or $env->{CONTENT_TYPE} =~ m{^(multipart/form-data)}i )
+        )
+    {
         my $ct = $1;
         my $token = $session->{$self->session_key}
             or return $self->token_not_found;
@@ -187,6 +189,16 @@ sub token_not_found {
             [ $body ]
         ];
     }
+}
+
+sub exclude {
+    my ( $self, $env ) = @_;
+    foreach my $regex ( @{ $self->exclude_paths } ) {
+        if ( $regex =~ $env->{PATH_INFO} ) {
+            return 1;
+        }
+    }
+    return 0;
 }
 
 1;
